0c65d6f6eba3b5a5c6fe68494b78185fc45cc821
Praharsh <praharsh922@gmail.com>
2018-12-07 15:52:13 +0530

    redux integrated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import App, { Container } from 'next/app';
-import Head from 'next/head';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import JssProvider from 'react-jss/lib/JssProvider';
-import getPageContext from '../src/getPageContext';
+import withRedux from 'next-redux-wrapper';
+import { Provider } from 'react-redux';
+
+
+import initStore from 'Root/src/store';
+import getPageContext from 'Root/src/getPageContext';
 
 class MyApp extends App {
 	constructor(props) {
@@ -21,33 +25,33 @@ class MyApp extends App {
 	}
 
 	render() {
-		const { Component, pageProps } = this.props;
+		const { Component, pageProps, store } = this.props;
 		return (
 			<Container>
-				<Head>
-					<title>My page</title>
-				</Head>
-				{/* Wrap every page in Jss and Theme providers */}
-				<JssProvider
-					registry={this.pageContext.sheetsRegistry}
-					generateClassName={this.pageContext.generateClassName}
-				>
-					{/* MuiThemeProvider makes the theme available down the React
-              tree thanks to React context. */}
-					<MuiThemeProvider
-						theme={this.pageContext.theme}
-						sheetsManager={this.pageContext.sheetsManager}
+				<Provider store={store}>
+					{/* Wrap every page in Jss and Theme providers */}
+					<JssProvider
+						registry={this.pageContext.sheetsRegistry}
+						generateClassName={this.pageContext.generateClassName}
 					>
-						{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-						<CssBaseline />
-						{/* Pass pageContext to the _document though the renderPage enhancer
+						{/* MuiThemeProvider makes the theme available down the React
+              tree thanks to React context. */}
+						<MuiThemeProvider
+							theme={this.pageContext.theme}
+							sheetsManager={this.pageContext.sheetsManager}
+						>
+							{/* CssBaseline kickstart an elegant,
+								consistent, and simple baseline to build upon. */}
+							<CssBaseline />
+							{/* Pass pageContext to the _document though the renderPage enhancer
                 to render collected styles on server side. */}
-						<Component pageContext={this.pageContext} {...pageProps} />
-					</MuiThemeProvider>
-				</JssProvider>
+							<Component pageContext={this.pageContext} {...pageProps} />
+						</MuiThemeProvider>
+					</JssProvider>
+				</Provider>
 			</Container>
 		);
 	}
 }
 
-export default MyApp;
+export default withRedux(initStore)(MyApp);
